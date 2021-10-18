38554978ae9822ddac1724da7a7e6e8a87379bd8
Yehra <yehra@pop-os.localdomain>
2021-10-19 00:20:00 +0200

    orginal commit

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
-import * as wasm from "hello-wasm-pack";
+import * as wasm from "jdrp-wasm";
 
-wasm.greet();
+// wasm.greet();
+
+const map_button = document.getElementById("map_button");
+const map = document.getElementById("map");
+
+const caracter_button = document.getElementById("caracter_button");
+const caracter = document.getElementById("caracter");
+
+const bag_button = document.getElementById("bag_button");
+const bag = document.getElementById("bag");
+
+map_button.addEventListener("click", (e) => {
+    e.preventDefault();
+    toogleUi(map.style.display, map, map_button);
+});
+
+caracter_button.addEventListener("click", (e) => {
+    e.preventDefault();
+    toogleUi(caracter.style.display, caracter, caracter_button);
+});
+
+bag_button.addEventListener("click", (e) => {
+    e.preventDefault();
+    toogleUi(bag.style.display, bag, bag_button);
+});
+
+
+const toogleUi = (d, t, b) => {
+    if(d == "none"){
+        b.classList.add("down");
+        t.style.display = "block";
+        t.style.zIndex += 10
+    }else{
+        b.classList.remove("down");
+        t.style.display = "none";
+        t.style.zIndex -= 10
+    }
+}
+
+window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+      return;
+    }
+  
+    switch (event.key) {
+      case "c":
+        toogleUi(caracter.style.display, caracter, caracter_button);
+        break;
+      case "b":
+        toogleUi(bag.style.display, bag, bag_button);
+        break;
+      case "m":
+        toogleUi(map.style.display, map, map_button);
+        break;
+      default:
+        return; 
+    }
+    event.preventDefault();
+  }, true);
+
+const progress = document.getElementById("xp_bar");
+RPGUI.set_value(progress, wasm.xp_state());
