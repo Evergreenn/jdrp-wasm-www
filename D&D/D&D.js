ae25f6eb68403671e858cb79feb4ca2cd85df701
guillaume <guillaume@grytics.com>
2021-10-20 06:03:34 +0200

    add d&d

---
+++ D&D/D&D.js
@@ -0,0 +1,76 @@
+export function DandD() {
+
+    const handleDragStart = (ev) => {
+        const j = JSON.stringify({ "id": ev.target.id, "class": ev.target.classList })
+        ev.dataTransfer.setData('text/plain', j);
+        ev.dataTransfer.effectAllowed = 'move';
+    }
+
+    const handleDragEnd = (ev) => {
+        ev.preventDefault();
+    }
+
+    const handleDragOver = (ev) => {
+        ev.preventDefault();
+        ev.dataTransfer.dropEffect = "move"
+
+    }
+
+    const handleDragEnter = (ev) => {
+        ev.preventDefault();
+    }
+
+    const handleDragLeave = (ev) => {
+        ev.preventDefault();
+
+    }
+
+    function handleDrop(ev) {
+        ev.stopPropagation(); // stops the browser from redirecting.
+        ev.preventDefault();
+        var dragObj = JSON.parse(ev.dataTransfer.getData("text"));
+
+        const draggableElement = document.getElementById(dragObj.id);
+        const dropzone = ev.target;
+
+        if (dropzone.offsetParent.id === "bag") {
+            if (dropzone.id == "") {
+                dropzone.appendChild(draggableElement);
+            } else {
+                exchangeElements(draggableElement, dropzone)
+            }
+        } else {
+            if (dropzone.id == "") {
+                if (dropzone.classList.contains("potion-slot")) {
+                    dropzone.appendChild(draggableElement)
+                }
+
+            } else {
+                if (dropzone.parentElement.classList.contains("potion-slot")) {
+                    exchangeElements(draggableElement, dropzone)
+                }
+            }
+        }
+    }
+
+    const exchangeElements = (element1, element2) => {
+        const clonedElement1 = element1.cloneNode(true);
+        const clonedElement2 = element2.cloneNode(true);
+
+        element2.parentNode.replaceChild(clonedElement1, element2);
+        element1.parentNode.replaceChild(clonedElement2, element1);
+
+        return clonedElement1;
+    }
+
+    let items = document.querySelectorAll('.drag');
+    items.forEach(function (item) {
+        item.addEventListener('dragstart', handleDragStart);
+        item.addEventListener('dragend', handleDragEnd);
+        item.addEventListener('dragenter', handleDragEnter);
+        item.addEventListener('dragleave', handleDragLeave);
+        item.addEventListener('dragover', handleDragOver);
+        item.addEventListener('drop', handleDrop);
+    });
+
+}
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import * as wasm from "jdrp-wasm";
 import * as Ui from "./UI/ui.js"
+import * as D from "./D&D/D&D.js";
 
 Ui.UI();
+D.DandD();
 
 window.addEventListener('load', () => {
     const progress = document.getElementById("xp_bar");
     RPGUI.set_value(progress, wasm.xp_state());
   }, false);
+
 
